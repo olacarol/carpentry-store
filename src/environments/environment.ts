f932569793fb5108fdf732984791936b4d0414c2
Carolina Barreto <caru.barreto@gmail.com>
2026-05-06 13:23:58 +0200

    add environment config

---
+++ src/environments/environment.ts
@@ -0,0 +1,9 @@
+export const environment = {
+    production: true,
+    emailjs: {
+      serviceID: 'service_lr41waz',
+      templateAdmin: 'template_gfkgpts',
+      templateClient: 'template_vz9xk4v',
+      publicKey: 'xh-ZpyYSJN2Yucj0w',
+    }
+  };
